4342be05935b1bf27e68a71aab9194736d575c6e
yingfeng-iu <yingfeng@iu.edu>
2020-03-02 17:19:22 -0500

    AMP-562/538: add tool wrapper

--- tools/hmgm/hmgm_main.py
+++ tools/hmgm/hmgm_main.py
@@ -1,23 +1,32 @@
+#!/usr/bin/env python3
+import os
+import os.path
+import sys
 
-class HmgmBase:
-    """Abstract base class defining the API for all HMGM implementations, which are based on task management platforms."""
-    
-    def main():
-        (output_file) = sys.argv[1]
-        try:
-            if task_exists() == False:
-                create_task()
-                exit(1) 
+# class HmgmBase:
+#     """Abstract base class defining the API for all HMGM implementations, which are based on task management platforms."""
+
+# Usage: hmgm_main.py [hmgm_type] [hmgm_log_path] [context] [input_json] [output_json] [hmgm_task] 
+def main():
+    (output_file) = sys.argv[1]
+    try:
+        if task_created() == False:
+            create_task()
+            exit(1) 
+        else:
+            if task_completed():
+                close_task()
+                write_output_file(output_file) # For testing only
+                exit(0)
             else:
-                if task_completed():
-                    close_task()
-                    write_output_file(output_file) # For testing only
-                    exit(0)
-                else:
-                    exit(1)        
-        except Exception as e:
-            exit(-1)
+                exit(1)        
+    except Exception as e:
+        exit(-1)
 
-    def task_completed():
-       
-        return True
+# Return true if HMGM task already created, i.e. the file containing the HMGM task info exists
+def task_created():
+    return os.path.exists(argv[5])
+
+def task_completed():
+   
+    return True
