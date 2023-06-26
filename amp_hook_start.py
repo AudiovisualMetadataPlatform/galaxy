371ae13e7a45295b89b93ca30f23cd01692be2cd
Brian Wheeler <bdwheele@iu.edu>
2023-06-26 12:18:53 -0400

    remove integrated_tool_panel.xml on startup

--- amp_hook_start.py
+++ amp_hook_start.py
@@ -8,6 +8,7 @@ from amp.config import load_amp_config
 from amp.logging import setup_logging
 import logging
 import subprocess
+from pathlib import Path
 
 def main():
     parser = argparse.ArgumentParser()
@@ -15,6 +16,13 @@ def main():
     args = parser.parse_args()
 
     setup_logging(None, args.debug)
+
+    # There's an integrated_tool_panel.xml that will sometimes
+    # remember previous configuration settings for the tool
+    # panel.  Remove the file and let galaxy rebuild it.
+    itp = Path(os.environ['AMP_ROOT'], 'galaxy', 'config', 'integrated_tool_panel.xml')
+    if itp.exists():
+        itp.unlink()
 
     try:
         subprocess.run([f"{os.environ['AMP_ROOT']}/galaxy/run.sh", "start"], check=True)
