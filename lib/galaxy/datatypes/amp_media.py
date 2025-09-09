2cc6dfb6f77b55c25e14cf9a1016ebe4c2c45f9e
yingfeng <yingfeng@iu.edu>
2025-09-08 23:22:42 -0400

    AMP-3841: - reorder sniffers to be closer to sample file - apply customization to media/text data type extensions

---
+++ lib/galaxy/datatypes/amp_media.py
@@ -0,0 +1,70 @@
+from galaxy.datatypes.media import Wav
+
+
+###########################
+# AMP extended media types
+###########################
+
+# class AudioVideo(Binary):
+#     """Class describing an audio/video binary file"""
+#     file_ext = "av"
+#     label = "Audio/Video"
+# 
+#     def sniff(self, filename):
+#         mt = subprocess.check_output(['file', '--mime-type', filename])
+#         return  mt.find("audio/")>=0 or mt.find("video/")>=0
+#     
+#     def set_peek(self, dataset, is_multi_byte=False):
+#         if not dataset.dataset.purged:
+#             dataset.peek = label
+#             dataset.blurb = nice_size(dataset.get_size())
+#         else:
+#             dataset.peek = 'file does not exist'
+#             dataset.blurb = 'file purged from disk'
+# 
+#     def display_peek(self, dataset):
+#         try:
+#             return dataset.peek
+#         except Exception:
+#             return self.label + " file (%s)" % (nice_size(dataset.get_size()))
+#
+# class Audio(AudioVideo):
+#     """Class describing an audio file"""
+#     file_ext = "audio"
+#     label = "Audio"
+# 
+#     def sniff(self, filename):
+#         mt = subprocess.check_output(['file', '--mime-type', filename])
+#         return  mt.find("audio/")>=0
+# 
+# class Video(AudioVideo):
+#     """Class describing a video file"""
+#     file_ext = "video"
+#     label = "Video"
+# 
+#     def sniff(self, filename):
+#         mt = subprocess.check_output(['file', '--mime-type', filename])
+#         return  mt.find("video/")>=0
+# 
+# class Wav(Audio):
+#     """Class describing a WAV audio file"""
+#     file_ext = "wav"
+#     label = "WAV"
+# 
+#     def sniff(self, filename):
+#         mt = subprocess.check_output(['file', '--mime-type', filename])
+#         return  mt.find("audio/wave")>=0 or mt.find("audio/wav")>=0 or mt.find("audio/x-wav")>=0 or mt.find("audio/x-pn-wav")>=0
+
+
+class Music(Wav):
+    """Class describing an AMP music WAV file"""
+    file_ext = "music"
+    label = "AMP Music WAV"
+
+
+class Speech(Wav):
+    """Class describing an AMP speech WAV file"""
+    file_ext = "speech"
+    label = "AMP Speech WAV"
+
+
